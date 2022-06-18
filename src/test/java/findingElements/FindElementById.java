05f2e44b49e7c7e4c36c4094e7ec2f1f4f924338
shaheen <mohammed_shaheen22@yahoo.com>
2022-06-18 20:28:25 +0200

    Try to use By Class to find elements

--- src/test/java/findingElements/FindElementById.java
+++ src/test/java/findingElements/FindElementById.java
@@ -28,8 +28,20 @@ public class FindElementById {
         System.out.println(userName.getAttribute("id"));
     }
 
+    @Test
+    public void findIdElementUsingByClass() {
+        By userName = By.id("username");
+        By password = By.id("password");
+        WebElement usernameElement = chromeDriver.findElement(userName);
+        WebElement passwordElement = chromeDriver.findElement(password);
+        System.out.println(usernameElement.getTagName());
+        System.out.println(passwordElement.getTagName());
+        System.out.println(usernameElement.getAttribute("id"));
+    }
+
     @AfterTest
     public void closeBrowser() {
         chromeDriver.quit();
     }
 }
+
